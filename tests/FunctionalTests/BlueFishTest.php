0a9f327daa2c9950c79f55ffc39da3064585a79f
Jesse Rushlow <jr@geeshoe.com>
2019-01-11 07:54:59 -0500

    Test database setup logic for functional tests separated from test logic.
    Allows for better code reuse in future tests.

--- tests/FunctionalTests/BlueFishTest.php
+++ tests/FunctionalTests/BlueFishTest.php
@@ -23,11 +23,11 @@
 namespace Geeshoe\BlueFish\Tests\FunctionalTests;
 
 use Geeshoe\BlueFish\Exceptions\BlueFishException;
+use Geeshoe\BlueFish\Tests\DBSetupForFuncTests;
 use Geeshoe\BlueFish\Users\BlueFish;
 use Geeshoe\BlueFish\Users\User;
 use Geeshoe\DbLib\Core\PreparedStatements;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 /**
  * Class BlueFishTest
@@ -36,10 +36,7 @@ use Ramsey\Uuid\Uuid;
  */
 class BlueFishTest extends TestCase
 {
-    /**
-     * @var \PDO
-     */
-    protected $pdo;
+    use DBSetupForFuncTests;
 
     /**
      * @var PreparedStatements
@@ -47,27 +44,11 @@ class BlueFishTest extends TestCase
     protected $preparedStatement;
 
     /**
-     * @throws \Exception
-     * {@inheritdoc}
+     * @inheritdoc
      */
     protected function setUp()
     {
-        $host = getenv('GSD_BFTD_HOST');
-        $port = getenv('GSD_BFTD_PORT');
-
-        $this->pdo = new \PDO(
-            'mysql:host=' . $host . ';port=' . $port,
-            getenv('GSD_BFTD_USER'),
-            getenv('GSD_BFTD_PASSWORD')
-        );
-
-        $this->createTempDB();
-        $this->pdo->exec('USE ' . getenv('GSD_BFTD_DATABASE').';');
-        $this->createTestTable();
-
-        $this->preparedStatement = new PreparedStatements($this->pdo);
-
-        $this->insertTestUser();
+        $this->preparedStatement = $this->getDbSetup();
     }
 
     /**
@@ -75,107 +56,7 @@ class BlueFishTest extends TestCase
      */
     protected function tearDown()
     {
-        $this->removeTestSchema();
-        $this->pdo = null;
-    }
-
-    /**
-     * @return int
-     */
-    protected function removeTestSchema(): int
-    {
-        return $this->pdo->exec(
-            'DROP SCHEMA`' . getenv('GSD_BFTD_DATABASE') . '`;'
-        );
-    }
-
-    /**
-     * @return int
-     */
-    protected function createTestSchema(): int
-    {
-        return $this->pdo->exec(
-            'CREATE SCHEMA`' . getenv('GSD_BFTD_DATABASE') . '`;'
-        );
-    }
-
-    protected function createTempDB(): void
-    {
-        $result = $this->createTestSchema();
-
-        if ($result !== 1) {
-            $this->removeTestSchema();
-            $result = $this->createTestSchema();
-        }
-
-        if ($result !== 1) {
-            throw new \PDOException(
-                'Unable to create test schema.'
-            );
-        }
-    }
-
-    protected function createTestTable(): void
-    {
-        $sql = file_get_contents(dirname(__DIR__, 2) . '/blueFishTables.sql');
-        $this->pdo->exec($sql);
-    }
-
-    /**
-     * @return array
-     * @throws \Exception
-     */
-    protected function insertTestRoleStatus(): array
-    {
-        $role = [
-            'id' => Uuid::uuid4()->toString(),
-            'role' => 'test'
-        ];
-
-        $status = [
-            'id' => Uuid::uuid4()->toString(),
-            'status' => 'test'
-        ];
-
-        $this->preparedStatement->executePreparedInsertQuery(
-            'BF_Roles',
-            $role
-        );
-
-        $this->preparedStatement->executePreparedInsertQuery(
-            'BF_Status',
-            $status
-        );
-
-        return ['role' => $role['id'], 'status' => $status['id']];
-    }
-
-    /**
-     * @throws \Exception
-     */
-    protected function insertTestUser(): void
-    {
-        $roleStatusUUIDs = $this->insertTestRoleStatus();
-
-        $user = new User();
-        $user->id = Uuid::uuid4()->toString();
-        $user->username = 'testName';
-        $user->password = password_hash('password', PASSWORD_DEFAULT);
-        $user->displayName = 'TestingAdmin';
-        $user->role = $roleStatusUUIDs['role'];
-        $user->status = $roleStatusUUIDs['status'];
-
-        $this->preparedStatement->executePreparedInsertQuery(
-            'BF_Users',
-            [
-                'id' => $user->id,
-                'username' => $user->username,
-                'password' => $user->password,
-                'displayName' => $user->displayName,
-                'role' => $user->role,
-                'status' => $user->status
-            ]
-        );
+        $this->tearDownDB();
     }
 
     /**
