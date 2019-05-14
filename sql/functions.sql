de4672ef401fda2e03889ba1df54df1311cc321a
Jesse Rushlow <jr@geeshoe.com>
2019-05-14 17:47:11 -0400

    Added test coverage for MySQL functions

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,24 +1,19 @@
 CREATE OR REPLACE FUNCTION UuidToBin(_uuid BINARY(36))
-            RETURNS BINARY(16)
-            LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
-            RETURN
-            UNHEX(CONCAT(
-                  SUBSTR(_uuid, 15, 4),
-            SUBSTR(_uuid, 10, 4),
-            SUBSTR(_uuid,  1, 8),
-            SUBSTR(_uuid, 20, 4),
-            SUBSTR(_uuid, 25) )
-  );
+    RETURNS BINARY(16)
+    LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+    RETURN
+        UNHEX(REPLACE(_uuid, '-', ''));
+
 
 CREATE OR REPLACE FUNCTION UuidFromBin(_bin BINARY(16))
-            RETURNS BINARY(36)
-            LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
-            RETURN
-            LCASE(CONCAT_WS('-',
-                  HEX(SUBSTR(_bin,  5, 4)),
-            HEX(SUBSTR(_bin,  3, 2)),
-            HEX(SUBSTR(_bin,  1, 2)),
-            HEX(SUBSTR(_bin,  9, 2)),
-            HEX(SUBSTR(_bin, 11))
-            )
-  );
+RETURNS BINARY(36)
+LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+RETURN
+    LCASE(CONCAT_WS('-',
+         SUBSTR(HEX(_bin), 1, 8),
+         SUBSTR(HEX(_bin), 9, 4),
+         SUBSTR(HEX(_bin), 13, 4),
+         SUBSTR(HEX(_bin), 17, 4),
+         SUBSTR(HEX(_bin), 21)
+
+));
